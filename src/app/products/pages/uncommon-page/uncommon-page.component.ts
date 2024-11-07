0a7ca84a8b23fa2cb4a3a4d52c9f2f4c6448f3db
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2024-11-06 22:09:57 -0600

    Uso del pipe i18nSelectPipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  // i18n select
+  public name: string = 'Fernando';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla'
+  }
+
+  changeClient(): void {
+    this.name = 'Mellisa';
+    this.gender = 'female';
+  }
 }
