4e589376c05bb355ae024f991a943b2a7c1941c0
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2024-11-07 22:21:24 -0600

    Async Pipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -39,4 +40,14 @@ export class UncommonPageComponent {
     age: 36,
     address: 'Ottawa, Canada'
   }
+
+  // Async Pipe
+  public myObservableTimer = interval(2000)//cada 2 segundos
+    .pipe(
+      tap( value => console.log('tap', value)
+      )
+    )
+
+
+
 }
