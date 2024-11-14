c72246e9f9b3f54a5d3695e48af1046678c0f947
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2024-11-13 21:13:02 -0600

    Argumentos dinamicos a pipes personalizados

--- src/app/products/pages/order/order.component.ts
+++ src/app/products/pages/order/order.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
 })
 export class OrderComponent {
 
+  public isUpperCase: boolean = false;
+
+  toggleUpperCase():void{
+    this.isUpperCase = !this.isUpperCase;//si es true lo cambia a false y vicebersa
+  }
 }
